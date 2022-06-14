f3dd8fdfbdeea98042a830963ce505507525f065
inotite <alan.zhou904@gmail.com>
2022-06-14 16:22:01 -0400

    feat: add gap at the end of the contract to avoid shifting down storage in inheritance chain

--- contracts/DeltaNeutralStableVolatileFactoryUpgradeable.sol
+++ contracts/DeltaNeutralStableVolatileFactoryUpgradeable.sol
@@ -153,4 +153,11 @@ contract DeltaNeutralStableVolatileFactoryUpgradeable is IDeltaNeutralStableVola
         depositFee = newDepositFee;
         emit DepositFeeSet(depositFee);
     }
+
+    /**
+     * @dev This empty reserved space is put in place to allow future versions to add new
+     * variables without shifting down storage in the inheritance chain.
+     * See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
+     */
+    uint256[50] private __gap;
 }
